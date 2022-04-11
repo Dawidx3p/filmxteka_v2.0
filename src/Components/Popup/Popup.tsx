dfc533e25f308b55b0efc2c80cf0b1c6a63f28b6
Dawidx3p <maildawcio@gmail.com>
2022-04-11 20:44:07 +0200

    update head

---
+++ src/Components/Popup/Popup.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Popup = () => {
+    return (
+        <article className="popup">
+            <section>
+                <nav>
+                    <button>x</button>
+                </nav>
+            </section>
+        </article>
+    )
+}
+
+export default Popup;
